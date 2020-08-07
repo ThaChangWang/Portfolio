ef1da2e4c4a54d7e09071ef0bc1d727837d187f6
Anders Bergquist <andersbergquist@Anderss-MacBook-Pro.local>
2020-08-07 10:57:59 -0700

    Just starting

--- src/main.js
+++ src/main.js
@@ -1,36 +1,41 @@
 import React from "react"
 import Login from "./login"
+import Home from "./home"
 
 class Main extends React.Component {
   constructor() {
     super()
     this.state = {
-      page: "main"
+      page: "home"
     }
   }
 
 
   render() {
 
-    if (this.state.page === "main") {
+    if (this.state.page === "home") {
 
       return (
         <div>
 
-          <button onClick={() => this.setState({ page: "login" })}>login</button>
-          <button onClick={() => this.setState({ page: "register" })}>register</button>
+          <button onClick={() => this.setState({ page: "home" })}>home</button>
+          <button onClick={() => this.setState({ page: "about" })}>about</button>
+          <button onClick={() => this.setState({ page: "projects" })}>projects</button>
+
+          <Home />
 
         </div>
       )
     }
 
-    else if (this.state.page === "login") {
+    else if (this.state.page === "about") {
 
       return (
         <div>
 
-          <button onClick={() => this.setState({ page: "login" })}>login</button>
-          <button onClick={() => this.setState({ page: "register" })}>register</button>
+          <button onClick={() => this.setState({ page: "home" })}>home</button>
+          <button onClick={() => this.setState({ page: "about" })}>about</button>
+          <button onClick={() => this.setState({ page: "projects" })}>projects</button>
 
           <Login />
 
@@ -38,13 +43,14 @@ class Main extends React.Component {
       )
     }
 
-    else if (this.state.page === "register") {
+    else if (this.state.page === "projects") {
 
       return (
         <div>
 
-          <button onClick={() => this.setState({ page: "login" })}>login</button>
-          <button onClick={() => this.setState({ page: "register" })}>register</button>
+          <button onClick={() => this.setState({ page: "home" })}>home</button>
+          <button onClick={() => this.setState({ page: "about" })}>about</button>
+          <button onClick={() => this.setState({ page: "projects" })}>projects</button>
 
           <h1>{this.state.page}</h1>        
 
